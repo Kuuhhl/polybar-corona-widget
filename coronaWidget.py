967438bf814b745992d28e58131a6cd9bdc90fa8
Philipp Landmann <landmann.philipp@gmx.de>
2021-05-28 12:40:19 +0200

    Fixed bug when parsing the data from the API.
    The bug caused completely wrong values to be displayed.
    
    When specifying a province, it would just pull old data.
    When not specifying one, it would produce really large numbers as it
    would add all of the historical data together.

--- coronaWidget.py
+++ coronaWidget.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
-import argparse, json, urllib.request, os, locale
+import argparse, json, urllib.request, os, locale, sys
+
+
+def get_api_json(url: str):
+    # Get the API-Response as JSON
+    with urllib.request.urlopen(url) as response:
+        json_response = json.loads(response.read().decode("utf-8"))
+        response_code = response.getcode()
+    if response_code != 200:
+        raise ConnectionError
+    return json_response
+
+
+def get_cache(cachePath):
+    with open(cachePath, "r") as f:
+        data = json.loads(f.read())
+        number = data["number"]
+        arrow = data["arrow"]
+    return {"number": number, "arrow": arrow}
+
 
 # Create the parser
 my_parser = argparse.ArgumentParser()
@@ -16,6 +35,7 @@ my_parser.add_argument(
     help="Specify the province of the coutry you want to display.",
     dest="province",
 )
+
 my_parser.add_argument(
     "-pre",
     "--prefix",
@@ -23,6 +43,7 @@ my_parser.add_argument(
     dest="prefix",
     default="",
 )
+
 my_parser.add_argument(
     "-suf",
     "--suffix",
@@ -30,6 +51,7 @@ my_parser.add_argument(
     dest="suffix",
     default="",
 )
+
 my_parser.add_argument(
     "-ar",
     "--enable-arrow",
@@ -37,6 +59,7 @@ my_parser.add_argument(
     dest="arrow",
     action="store_true",
 )
+
 my_parser.add_argument(
     "-up",
     "--up-arrow",
@@ -44,6 +67,7 @@ my_parser.add_argument(
     dest="upArrow",
     default=" ↗",
 )
+
 my_parser.add_argument(
     "-dw",
     "--down-arrow",
@@ -51,6 +75,7 @@ my_parser.add_argument(
     dest="downArrow",
     default=" ↘",
 )
+
 my_parser.add_argument(
     "-lo",
     "--enable-locale",
@@ -58,6 +83,7 @@ my_parser.add_argument(
     dest="locale",
     action="store_true",
 )
+
 # Parse arguments
 args = my_parser.parse_args()
 
@@ -69,12 +95,15 @@ cachePath = os.path.join(
         "coronaCache",
     ),
 )
+
 # Check cache file integrity:
 # Create new file with fallback entries
 # if it has bad values or if it doesn't exist yet.
 try:
     with open(cachePath, "r") as f:
         cachedData_json = json.loads(f.read())
+
+    # make sure that values have the right data-type
     int(cachedData_json["number"])
     bool(cachedData_json["arrow"])
 
@@ -88,7 +117,8 @@ except:
     # Create path for the file if we have to
     if not os.path.exists(os.path.dirname(cachePath)):
         os.makedirs(os.path.dirname(cachePath))
-    # Write fallback value
+
+    # Write fallback values
     with open(cachePath, "w") as f:
         f.write(
             json.dumps(
@@ -96,73 +126,69 @@ except:
                     "country": args.country,
                     "province": args.province,
                     "number": 0,
-                    "arrow": f"{bool(True)}",
+                    "arrow": True,
                 }
             )
         )
 
+# get data from API
+try:
+    if args.province:
+        response = get_api_json(
+            f"https://api.covid19api.com/live/country/{args.country}/status/confirmed"
+        )
 
-# Get the API-Response as JSON
-with urllib.request.urlopen(
-    f"https://api.covid19api.com/live/country/{args.country}/status/confirmed"
-) as response:
-    json_response = json.loads(response.read().decode("utf-8"))
-    response_code = response.getcode()
+        # get latest value for given province
+        found = False
+        for timestamp in response:
+            if timestamp["Province"] == args.province:
+                number = timestamp["Active"]
+                found = True
+        if found == False:
+            raise Exception("Province not found!")
+    else:
+        response = get_api_json(
+            f"https://api.covid19api.com/total/country/{args.country}"
+        )
 
-if response_code != 200:
+        # get latest item
+        latest = response[len(response) - 1]
+        number = latest["Active"]
+
+except ConnectionError:
     # If we have no internet / the API is down
     # we use the cached file to fetch the data.
-    with open(cachePath, "r") as f:
-        cachedData_json = json.loads(f.read())
-        cachedNumber = cachedData_json["number"]
-        cachedArrow = cachedData_json["arrow"]
+    cache = get_cache(cachePath)
+
+    number = cache["number"]
+    arrow = cache["arrow"]
+
     # Print our string to polybar
-    print(f"{args.prefix}{cachedNumber}{cachedArrow}{args.suffix}")
+    print(f"{args.prefix}{number}{arrow}{args.suffix}")
+
     # Don't continue script
-    exit()
-
-# We Parse the JSON here
-if args.province:
-    province = True
-    # Get provice's case numbers if we provided one in arguments
-    for province in json_response:
-        if province["Province"] == args.province:
-            number = province["Active"]
-            break
-else:
-    province = False
-    # Get numbers of all of the country:
-    # add together all cases in provinces of country
-    number = 0
-    for province in json_response:
-        number += province["Active"]
-
+    sys.exit()
 
 # Load data from cache to have
 # something to compare to
-with open(cachePath, "r") as f:
-    cachedData_json = json.loads(f.read())
-    cachedNumber = cachedData_json["number"]
-    cachedArrowBool = bool(cachedData_json["arrow"])
+cache = get_cache(cachePath)
 
 # Get the correct arrow to display
 # arrowBool: True is up; False is down
-if cachedNumber == number:
-    arrowBool = cachedArrowBool
+if cache["number"] == number:
+    arrowBool = cache["arrow"]
+elif number > cache["number"]:
+    arrowBool = True
 else:
-    if number > cachedNumber:
-        arrowBool = True
+    arrowBool = False
 
-    elif number < cachedNumber:
-        arrowBool = False
-
-# Get the correct arrow symbol
-if arrowBool == True:
+# Get the correct arrow symbol from arguments
+if arrowBool:
     arrowStr = args.upArrow
 else:
     arrowStr = args.downArrow
 
-# Save cache to file
+# Save new cache to file
 with open(cachePath, "w") as f:
     f.write(
         json.dumps(
@@ -170,14 +196,16 @@ with open(cachePath, "w") as f:
                 "country": args.country,
                 "province": args.province,
                 "number": number,
-                "arrow": f"{arrowBool}",
+                "arrow": arrowBool,
             }
         )
     )
-# Don't print arrow if not enabled
+
+# Don't print arrow if not enabled in arguments
 if not args.arrow:
     arrowStr = ""
-# Print with locale seperators if wanted
+
+# Print with locale seperators if enabled
 if args.locale:
     locale.setlocale(locale.LC_ALL, "")
     number = "{0:n}".format(number)
